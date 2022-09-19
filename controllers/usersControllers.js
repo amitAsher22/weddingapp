e79a3aac9ff945cf50eae32923be2910dbb0ef88
Nir Alfasi <alfasin@gmail.com>
2022-09-20 02:12:32 +0300

    fix code to return http response code as well

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,26 +3,20 @@ import UsersSchem from "../model/UsersModel.js";
 
 const getUsers = asyncHandler(async (req, res) => {
   const getusers = await UsersSchem.find();
-
-  res.json(getusers);
+  res.status(200).json(getusers);
 });
-const settUsers = asyncHandler(async (req, res) => {
-  const setUsers = await UsersSchem.create({
+
+const createUser = asyncHandler(async (req, res) => {
+  const user = await UsersSchem.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
 
-  res.json(setUsers);
+  res.status(200).json(user);
 });
 
 const updateUsers = asyncHandler(async (req, res) => {
-  const updateUser = await UsersSchem.findById(req.params.id);
-
-  // if (!updateUser) {
-  //   res.status(400);
-  //   throw new Error("Goal not found");
-  // }
   const updateuser = await UsersSchem.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -31,15 +25,13 @@ const updateUsers = asyncHandler(async (req, res) => {
     }
   );
 
-  res.json(updateuser);
+  res.status(200).json(updateuser);
 });
 
 const deleteUsers = asyncHandler(async (req, res) => {
   const deleteuser = await UsersSchem.findById(req.params.id);
-
   await deleteuser.remove();
-
   res.json({ message: `delete users with id:  ${req.params.id}` });
 });
 
-export { getUsers, settUsers, updateUsers, deleteUsers };
+export { getUsers, createUser, updateUsers, deleteUsers };
