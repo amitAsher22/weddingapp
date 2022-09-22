27da88ee846147802648574adf7086b0b0bc1227
amita <amitasher4@gmail.com>
2022-09-22 22:00:17 +0300

    delete comments from server > router > registerUserRouter

--- Router/registerUserRouter.js
+++ Router/registerUserRouter.js
@@ -1,13 +1,10 @@
 import express from "express";
-import { authValidation } from "../validation/authValidation.js";
-// import { loginUserValidation } from "../validation/loginValidation.js";
+import { authValidation } from "../validation/RegisterValidation.js";
 import { registerUser, loginUsers } from "../controllers/RegisterController.js";
-// import { verifyJWT } from "../controllers/RegisterController.js";
 
 const routers = express.Router();
 
 routers.post("/register", authValidation, registerUser);
 routers.post("/login", loginUsers);
-// routers.get("/verify", , verify);
 
 export default routers;
